85a09d9f5dd7d53962e65ee4293e3bdc19046e8b
Daniel Pereira <pereiradpg@yahoo.ca>
2017-07-13 15:31:27 -0400

    Correct errors and verify Passes verification and works properly

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-var picture = require('cat-picture')
-var image = require('lighting-image-poly')
-var remote = require('electron').remote
-var fs = require('fs')
+var remote = require('electron').remote;
+var fs = require('fs');
+var image = require('lightning-image-poly');
+var picture = require('cat-picture');
+var src = picture.src;
 
-function save () {
+picture.remove();
+
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+
+function save() {
   remote.getCurrentWindow().webContents.printToPDF({
-    portrait: true
-  }, function (err, data) {
-    fs.writeFile('annotation.pdf', data, function (err) {
-      if (err) alert('error generationg pdf! ' + err.message)
-      else alert ('pdf saved!')
-    })
-  })
+    portrait: true,
+  }, function(err, data) {
+    fs.writeFile('annotation.pdf', data, function(err) {
+      if (err) {
+        window.alert('error generating pdf! ' + err.message);
+      } else {
+        window.alert('pdf saved!');
+      }
+    });
+  });
 }
 
-window.addEventListener('keydown', function (e) {
-  if (e.keyCode == 80) save()
-})
-
-var src = picture.src
-picture.remove()
-
-var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode === 80) {
+    save();
+  }
+});
