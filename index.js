0eba73478b1c46f9ac0118307ae005c081214b28
Daniel Pereira <pereiradpg@yahoo.ca>
2017-07-13 15:14:20 -0400

    Define a function It will save the current window to a PDF

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var image = require('lighting-image-poly')
 var remote = require('electron').remote
 var fs = require('fs')
 
+function save () {
+  remote.getCurrentWindow().webContents.printToPDF({
+    portrait: true
+  }, function (err, data) {
+    fs.writeFile('annotation.pdf', data, function (err) {
+      if (err) alert('error generationg pdf! ' + err.message)
+      else alert ('pdf saved!')
+    })
+  })
+}
+
 var src = picture.src
 picture.remove()
 
